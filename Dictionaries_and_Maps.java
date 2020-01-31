d4e5fe4b39a22f7a89e4e8de93a5b0d84e23008e
bforkbist <59921267+bforkbist@users.noreply.github.com>
2020-02-01 00:23:32 +0530

    Create Dictionaries_and_Maps.java

---
+++ Dictionaries_and_Maps.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String, Integer> phonebook= new HashMap<String, Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phonebook.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(phonebook.get(s)!=null)   
+                System.out.println(s+"="+phonebook.get(s));
+            else
+              System.out.println("Not found");
+            
+        }
+        in.close();
+    }
+}
